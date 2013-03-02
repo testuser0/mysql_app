8a12053a470f11bbb15cec88b585cd1112907ad7
debuggie <support@debuggie.com>
2013-03-02 02:12:06 -0800

    Add Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysqlApp::Application.config.secret_token = '71ab03622d101332ccb3a3543848965b9988ba649f1d5eecda623c14103a612c4bfbac6c51f13bbf1e2d2baf33eed7c3779baad69be99323e145ca0d379c5bb4'
---
+++ test/unit/user_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+  test "the truth2" do
+    assert true
+  end
+  test "the truth3" do
+    assert true
+  end
+  test "the truth4" do
+    assert true
+  end
+  test "the truth5" do
+    assert true
+  end
+
+  test "creation" do
+    User.create!
+  end
+end
